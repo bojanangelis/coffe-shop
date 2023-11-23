2b5919a2e04d452cbdb9e9154735913713dc59e1
bojanangelis <bojanangeleski98@gmail.com>
2023-11-23 15:06:57 +0100

    init front-end

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,30 +1,30 @@
-import Image from 'next/image';
+import { gql } from '../data-access/graphql-client';
 
 export default async function Index() {
-  return (
-    <>
-      <figure className=" bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        <img
-          className="w-24 h-24 rounded-full mx-auto"
-          src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-          alt=""
-        />
-        <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-          <blockquote>
-            <p className="text-lg font-medium">
-              “Tailwind CSS is the only framework that I've seen scale on large
-              teams. Its easy to customize, adapts to any design, and the build
-              size is tiny.”
-            </p>
-          </blockquote>
-          <figcaption className="font-medium">
-            <div className="text-sky-500 dark:text-sky-400">Sarah Dayan</div>
-            <div className="text-slate-700 dark:text-slate-500">
-              Staff Engineer, Algolia
-            </div>
-          </figcaption>
-        </div>
-      </figure>
-    </>
-  );
+  const { users } = await gql.GetUsers();
+  return users.map((user) => (
+    <figure
+      key={user.id}
+      className=" bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800"
+    >
+      <img
+        className="w-24 h-24 rounded-full mx-auto"
+        src={`https://i.pravatar.cc/150?u=${user.id}`}
+        alt=""
+      />
+      <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
+        <blockquote>
+          <p className="text-lg font-medium">
+            “Tailwind CSS is the only framework that I've seen scale on large
+            teams. Its easy to customize, adapts to any design, and the build
+            size is tiny.”
+          </p>
+        </blockquote>
+        <figcaption className="font-medium">
+          <div className="text-sky-500 dark:text-sky-400">{user?.name}</div>
+          <div className="text-slate-700 dark:text-slate-500">{user.email}</div>
+        </figcaption>
+      </div>
+    </figure>
+  ));
 }
---
+++ libs/web/data-access-graphql/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/data-access.gql.gen';
+export * from './lib/types';
+export * from './lib/get-graphql-client';
