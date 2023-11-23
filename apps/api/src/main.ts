e6cbcf70494a38c4f9108e716a4b5cb8f958e979
bojanangelis <bojanangeleski98@gmail.com>
2023-11-23 12:13:55 +0100

    init backend

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,18 +5,25 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import helmet from '@fastify/helmet';
 import { AppModule } from './app/app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
   );
+
+  const port = process.env.PORT || 3333;
+  app.enableCors();
+  // TODO: contentSecurityPolicy should be turned ON in production
+  await app.register(helmet, { contentSecurityPolicy: false });
+  await app.listen(port);
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
