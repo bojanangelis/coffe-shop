3fc717f242a563f87fcde4f1b77ab41980ae1e65
bojanangelis <bojanangeleski98@gmail.com>
2023-11-24 13:56:23 +0100

    wip

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,13 +1,23 @@
 import './global.css';
-
+import { Inter } from 'next/font/google';
 export const metadata = {
   title: 'Starbucks',
-  description: 'best coffee shop out there!'
+  description:
+    'More than just great coffee. Explore the menu, sign up for Starbucks® Rewards, manage your gift card and more.',
+  icons: {
+    icon: [
+      {
+        url: '/starbucks.iso',
+        href: '/starbucks.iso'
+      }
+    ]
+  }
 };
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html className={inter.className} lang="en">
       <body>{children}</body>
     </html>
   );
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,20 @@
+import Image from 'next/image';
 import { gql } from '../data-access/graphql-client';
 import { webEnv } from '../environments/environment';
 
 const { storage } = webEnv;
 export default async function Index() {
-  const { users } = await gql.GetUsers();
   const { homeBlocks } = await gql.GetHomeImages();
-  console.log(homeBlocks);
+
   return homeBlocks.map((homeBlock) => (
     <figure key={homeBlock.id} className=" bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-      <img className="w-40 h-40" src={`${storage.url}/${homeBlock.imagePath}`} />
+      <Image
+        width={300}
+        height={200}
+        alt={homeBlock.title}
+        className="w-40 h-40"
+        src={`${storage.url}/${homeBlock.imagePath}`}
+      />
       <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
         <blockquote>
           <p className="text-lg font-medium">
